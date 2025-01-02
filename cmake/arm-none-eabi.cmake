c85ee433b402277b5ab6c08589ad96ad4703baa7
RiGuo Wu <1807332480@qq.com>
2025-01-02 23:56:06 +0800

    将编译工具链选项放到cmake目录下

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -1,26 +1,27 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-# Set toolchain paths
-set(TOOLCHAIN_PATH "D:/Arm GNU Toolchain arm-none-eabi/13.3 rel1/bin")
-set(TOOLCHAIN_PREFIX "${TOOLCHAIN_PATH}/arm-none-eabi-")
+# 查找工具链
+find_program(ARM_GCC arm-none-eabi-gcc.exe)
+get_filename_component(TOOLCHAIN_PATH ${ARM_GCC} DIRECTORY)
 
-# Set compilers
-set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
-set(CMAKE_ASM_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
-set(CMAKE_OBJCOPY "${TOOLCHAIN_PREFIX}objcopy.exe")
-set(CMAKE_SIZE "${TOOLCHAIN_PREFIX}size.exe")
+# 设置编译器
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/arm-none-eabi-gcc.exe)
+set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
+set(CMAKE_OBJCOPY ${TOOLCHAIN_PATH}/arm-none-eabi-objcopy.exe)
+set(CMAKE_SIZE ${TOOLCHAIN_PATH}/arm-none-eabi-size.exe)
 
+# MCU 标志
+set(CPU_FLAGS "-mcpu=cortex-m3 -mthumb")
+set(COMMON_FLAGS "${CPU_FLAGS} -Wall -fdata-sections -ffunction-sections")
+
+# 编译标志
+set(CMAKE_C_FLAGS "${COMMON_FLAGS}" CACHE STRING "C compiler flags")
+set(CMAKE_ASM_FLAGS "${COMMON_FLAGS}" CACHE STRING "ASM compiler flags")
+set(CMAKE_EXE_LINKER_FLAGS "${CPU_FLAGS} -specs=nano.specs -specs=nosys.specs -Wl,--gc-sections" CACHE STRING "Linker flags")
+
+# 寻找库和头文件设置
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-# MCU flags
-set(CPU "-mcpu=cortex-m3")
-set(FPU "")
-set(FLOAT_ABI "")
-
-set(CMAKE_C_FLAGS "${CPU} ${FPU} ${FLOAT_ABI} -mthumb -fdata-sections -ffunction-sections" CACHE STRING "C compiler flags")
-set(CMAKE_ASM_FLAGS "${CPU} ${FPU} ${FLOAT_ABI} -mthumb" CACHE STRING "ASM compiler flags")
-set(CMAKE_EXE_LINKER_FLAGS "${CPU} ${FPU} ${FLOAT_ABI} -mthumb -specs=nosys.specs -Wl,--gc-sections" CACHE STRING "Linker flags")
